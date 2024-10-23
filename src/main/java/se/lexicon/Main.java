584d0f9c4acae52a9296fa2d578725c04294cc87
Sayana Surendran <sayanasurendran31@gmail.com>
2024-10-23 11:51:41 +0200

    Added Name Repository Class and method implementation

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,17 +1,33 @@
 package se.lexicon;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Size of Array::"+ NameRepository.getSize());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Add names and display all the names available in Array.
+        NameRepository.add("Sayana Surendran");
+        NameRepository.add("Erik Svensson");
+        System.out.println("Displays all elements in Names Array:"+ Arrays.toString(NameRepository.findAll()));
+        System.out.println(NameRepository.add("Sayana Surendran"));
+
+        //Replaces the existing names in NamesArray with below names
+        NameRepository.setNames(new String[]{"Philip Thomas", "Ulf Svensson", "Simon Svensson", "Kent Svensson"});
+        System.out.println("Displays all elements in Names Array:"+ Arrays.toString(NameRepository.findAll()));
+
+        //prints if Name is available in Array, if not returns null
+        System.out.println("Name found ::"+NameRepository.find("Sayana Surendran"));
+        System.out.println("Name found ::"+NameRepository.find("Sayana"));
+        System.out.println("Name found ::"+NameRepository.find("Philip Thomas"));
+
+        //returns the size of Array
+        System.out.println("Size of Array::"+ NameRepository.getSize());
+
+        //returns empty array
+        NameRepository.clear();
+        System.out.println("Size of Array::"+ NameRepository.getSize());
+
     }
 }
--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,4 +1,52 @@
 package se.lexicon;
 
+import java.util.Arrays;
+
 public class NameRepository {
+
+    private static String[] names={};
+
+    public static int getSize(){
+        return names.length;
+    }
+
+    public static void setNames(String[] names){
+      NameRepository.names= Arrays.copyOf(names,names.length);
+      System.out.println("setNames = " + Arrays.toString(NameRepository.names));
+    }
+
+
+    public static void clear(){
+        names= new String[]{};
+
+
+    }
+
+    public static String[] findAll(){
+        return names;
+    }
+
+
+    public static String find(final String fullName){
+            for(String name:names) {
+            if (name.equalsIgnoreCase(fullName)) {
+                return name;
+            }
+        }
+          return null;
+    }
+
+    public static boolean add(final String fullName){
+
+       if(fullName.equalsIgnoreCase(find(fullName))){
+            return false;
+        }
+        names=Arrays.copyOf(names,names.length+1);
+        names[names.length -1]=fullName;
+        System.out.println("Names = " + Arrays.toString(names));
+        return true;
+    }
+
+
+
 }
